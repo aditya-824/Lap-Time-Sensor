a7f5365af32fc9681de72f02667ad94b73e29f0b
aditya-824 <aditya.bhalerao@somaiya.edu>
2025-05-28 18:50:52 +0530

    Added comments

--- Lap_Time_Sensor_V1/Lap_Time_Sensor_V1.ino
+++ Lap_Time_Sensor_V1/Lap_Time_Sensor_V1.ino
@@ -1,11 +1,14 @@
+/* BLYNK MANDATED */
 #define BLYNK_TEMPLATE_ID "TMPL3SyT65zC5"
 #define BLYNK_TEMPLATE_NAME "Lap Time Sensor"
 #define BLYNK_AUTH_TOKEN "9xpbf-HVRUJnluJW_mOhDdkJHhoJ5mn7"
 
+/* LIBRARIES */
 #include <stdbool.h>
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 
+/* TIME DEFINES */
 #define carDelay 5 // Delay for car to pass by in seconds
 #define startDelay 2 // Delay to prevent start of measurement at end of measurement, in seconds
 
@@ -13,24 +16,26 @@
 const int motionSensor = 14;
 
 // Timer: Auxiliary variables
-volatile unsigned long startTime = 0;
-volatile unsigned long endTime = 0;
-volatile boolean firstSignalDetected = false;
-volatile boolean timingComplete = false;
-volatile unsigned long lastDetectionTime = 0;
-volatile boolean timingStarted = false;
+volatile unsigned long startTime = 0; // Stores the start time
+volatile unsigned long endTime = 0; // Stores the end time
+volatile boolean firstSignalDetected = false; // Indicates whether high signal is first detected or not
+volatile boolean timingComplete = false; // Indicates whether one timing cycle has completed or not
+volatile boolean timingStarted = false; // Indicates when timing has started
+volatile unsigned long lastDetectionTime = 0; // Stores the latest measured time for timed high-signal deadbands
 
+/* WIFI PARAMETERS */
 char ssid[] = "Aditya's Phone";
 char password[] = "geO071!?";
 
-void ICACHE_RAM_ATTR detectsMovement() {
+
+void ICACHE_RAM_ATTR detectsMovement() { // Interrupt function
   unsigned long now = millis();
-  if (!firstSignalDetected && ((now - lastDetectionTime) >= startDelay*1000)) {
+  if (!firstSignalDetected && ((now - lastDetectionTime) >= startDelay*1000)) { // Starts timing if high signal is first in cycle and 2 seconds have passed since end of last cycle
     startTime = now;
     firstSignalDetected = true;
     lastDetectionTime = now;
     timingStarted = true;
-  } else if (now - lastDetectionTime >= carDelay*1000) {
+  } else if (now - lastDetectionTime >= carDelay*1000) { // Ends timing cycle if at least 5 seconds have passed since start of cycle
     endTime = now;
     firstSignalDetected = false;
     timingComplete = true;
@@ -44,15 +49,17 @@ void setup() {
   pinMode(motionSensor, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
-  Blynk.begin (BLYNK_AUTH_TOKEN, ssid, password);
+  Blynk.begin (BLYNK_AUTH_TOKEN, ssid, password); // NOTE: Code will not progress until Wi-Fi connection is established
 }
 
 void loop() {
   Blynk.run();
+
   if (timingStarted) {
     Serial.println("Timing started...");
     timingStarted = false;
   }
+
   if (timingComplete) {
     unsigned long duration = endTime - startTime;
     Serial.println("Timing stopped.");
@@ -60,6 +67,6 @@ void loop() {
     Serial.print(duration);
     Serial.println(" ms");
     timingComplete = false;
-    Blynk.virtualWrite(V0, duration);
+    Blynk.virtualWrite(V0, duration); // Sends 'duration' value to Blynk virtual pin
   }
 }
