4e4cc4e9613e0db462e7c15866c1426fcfa27699
Aditya Gandhi <aditya.bhalerao@somaiya.edu>
2025-05-30 16:17:01 +0530

    Improved readability

--- Lap_Time_Sensor_V3/Lap_Time_Sensor_V3.ino
+++ Lap_Time_Sensor_V3/Lap_Time_Sensor_V3.ino
@@ -2,23 +2,20 @@
 #include <stdbool.h>
 
 /* TIME DEFINES */
-#define carDelay 5 // Delay for car to pass by in seconds
-#define startDelay 5 // Delay to prevent start of measurement at end of measurement, in seconds
-#define MAX_LAPS 2 // Maximum number of laps that can be measured
+#define CAR_DELAY 5 // Delay for car to pass by, in seconds
+#define MAX_LAPS 2 // Number of laps to measure
 #define DISTANCE_THRES 10.0 // Distance between car and sensor at start/finish line
 
 /* PINS */
-// const int motionSensor = 14;
-const int TRIG_PIN = 8;
-const int ECHO_PIN = 9;
+const int TRIG_PIN = 8; // Ultrasonic sensor Trigger pin
+const int ECHO_PIN = 9; // Ultrasonic sensor Echo pin
 
-// Timer: Auxiliary variables
+/* VARIABLES */
 volatile unsigned long startTime = 0; // Stores the start time
 volatile unsigned long endTime = 0; // Stores the end time
 volatile boolean firstSignalDetected = false; // Indicates whether high signal is first detected or not
 volatile boolean timingComplete = false; // Indicates whether one timing cycle has completed or not
 volatile boolean timingStarted = false; // Indicates when timing has started
-int reset = 0; // Switch to reset timing code
 volatile unsigned long lastDetectionTime = 0; // Stores the latest measured time for timed high-signal deadbands
 float uTiming = 0.0; // Ultrasonic sensor timing
 float uDistance = 0.0; // Ultrasonic sensor distance
@@ -27,11 +24,7 @@ unsigned long slowestTime = 0; // Slowest lap time
 unsigned long lapTimes[MAX_LAPS] = {0}; // Array to store lap times
 volatile int lapCount = 0; // Number of laps recorded
 
-/* WIFI PARAMETERS */
-char ssid[] = "";
-char password[] = "";
-
-void detectsMovement() { // Interrupt function
+void detectsMovement() { // Timing function
   unsigned long now = millis();
   if (lapCount >= MAX_LAPS) return; // Stop detecting after set laps
 
@@ -40,7 +33,7 @@ void detectsMovement() { // Interrupt function
     firstSignalDetected = true;
     lastDetectionTime = now;
     timingStarted = true;
-  } else if (now - lastDetectionTime >= carDelay*1000) { // Ends timing cycle if at least 5 seconds have passed since start of cycle
+  } else if (now - lastDetectionTime >= CAR_DELAY*1000) { // Ends timing cycle if at least 5 seconds have passed since start of cycle
     endTime = now;
     // firstSignalDetected = false;
     timingComplete = true;
@@ -49,46 +42,27 @@ void detectsMovement() { // Interrupt function
   }
 }
 
-// BLYNK_WRITE(V1) // Blynk callback function to send value of Reset button
-// {
-//   reset = param.asInt(); // Get value as integer
-// }
-
 void setup() {
   Serial.begin(9600);
 
-  // pinMode(motionSensor, INPUT_PULLUP);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  // attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
 
   digitalWrite(TRIG_PIN, LOW);
-
-  // Blynk.begin (BLYNK_AUTH_TOKEN, ssid, password); // NOTE: Code will not progress until Wi-Fi connection is established
 }
 
 void loop() {
-  // Blynk.run();
-
+  /* Generating ultrasonic wave */
   digitalWrite(TRIG_PIN, LOW);
   delay(2);
   digitalWrite(TRIG_PIN, HIGH);
   delay(10);
-  digitalWrite(TRIG_PIN, LOW); // Generating ultrasonic wave
+  digitalWrite(TRIG_PIN, LOW);
   uTiming = pulseIn(ECHO_PIN, HIGH);
   uDistance = (uTiming * 0.034) / 2; // Calculating distance from sensor
 
-  if (reset) { // Resets timer if virtual pin is high
-    lapCount = 0;
-    firstSignalDetected = false;
-    reset = 0;
-    delay(5000); // Wait for virtual pin to reset
-    Serial.println("Timer reset.");
-    Serial.println("---------------------------------------------------------------------------");
-  }
-
   if (uDistance <= DISTANCE_THRES) {
-    detectsMovement();
+    detectsMovement(); // Calling function when car passes by
   }
 
   if (timingComplete && lapCount < MAX_LAPS) {
@@ -101,7 +75,6 @@ void loop() {
     Serial.print("Time: ");
     Serial.print(duration);
     Serial.println(" ms");
-    // Blynk.virtualWrite(V0, duration); // Send current lap time to Blynk
     lapCount++;
     timingComplete = false;
 
@@ -118,10 +91,10 @@ void loop() {
         Serial.print(": ");
         Serial.print(lapTimes[i]);
         Serial.println(" ms");
-        if(lapTimes[i] < fastestTime) {
+        if(lapTimes[i] < fastestTime) { // Calculating fastest lap
           fastestTime = lapTimes[i];
         }
-        if(lapTimes[i] > slowestTime) {
+        if(lapTimes[i] > slowestTime) { // Calculating slowest lap
           slowestTime = lapTimes[i];
         }
       }
@@ -130,7 +103,7 @@ void loop() {
       Serial.print("Slowest time: ");
       Serial.println(slowestTime);
       Serial.println("Reset module to restart timer.");
-      // Optionally, send all lap times to Blynk here
+      Serial.println("---------------------------------------------------------------------------");
     }
   }
 
